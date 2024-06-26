b921af61db9e3d1d4bb554263f1f6e2046a7cf8a
Sergij <stiv4113@gmail.com>
2024-06-26 12:08:25 +0300

    Finish task5

---
+++ task5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+
+namespace task5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Введіть логін");
+            string login = Console.ReadLine();
+
+            Console.WriteLine("Введіть пароль");
+            string password = Console.ReadLine();
+
+            if (CheckLogin(login)&& CheckPassword(password))
+            {
+                Console.WriteLine("Ура");
+            }
+            else
+            {
+                Console.WriteLine("Спробуй ще");
+            }
+        }
+       
+    }
+
+    static bool CheckLogin(string login)
+    {
+        return Regex.IsMatch(login, "^[a-zA-Z]+$");
+    }
+
+    static bool CheckPassword(string password)
+    {
+        return Regex.IsMatch(password, "^[0-9!@#$%^&*()_+=-]+$");
+    }
+}

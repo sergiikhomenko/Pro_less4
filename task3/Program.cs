6d58e7aea29e43ce0a17e86200ab564c79e27730
Sergij <stiv4113@gmail.com>
2024-06-26 11:27:46 +0300

    Finish task3

---
+++ task3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Mime;
+
+namespace task3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var prepositions = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+        {
+            "без", "в", "від", "до", "за", "з", "і", "й", "на", "по", "під", "про", "у", "через"
+        };
+        string path = "text.txt";
+        string newText;
+        if (File.Exists(path))
+        {
+            string text = File.ReadAllText(path);
+
+            string[] words = text.Split(new char[] { ' ', '\r', '\n', '\t' });
+
+            for (int  i = 0;  i < words.Length;  i++)
+            {
+                string clearWords = words[i].Trim('.', ',', ';', ':', '!', '?' );
+
+                if (prepositions.Contains(clearWords))
+                {
+                    words[i] = "ГАВ";
+                }
+            }
+
+            newText = string.Join(" ", words);
+            string newFail = "New_Text.txt";
+            File.WriteAllText(newFail,newText);
+        }
+        else
+        {
+            Console.WriteLine("Невдалось прочитати  файл");
+        }
+     
+    }
+}
